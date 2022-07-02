211aae2ef66b1f409340f461289466935b9610bd
luismts <deivi_0326@hotmail.com>
2022-07-02 17:38:34 -0400

    Add base sample

---
+++ samples/NavigationServiceSample/NavigationServiceSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NavigationServiceSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ samples/NavigationServiceSample/NavigationServiceSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigationServiceSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
